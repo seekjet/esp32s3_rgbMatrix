f03cb7296af8f32e89524b42d145ea66f1a4701c
Ryan <ryan@star-tek.com.au>
2022-09-21 14:39:02 +1000

    added access to gpios, panel dims, and spi clock to header file

--- panelDriver.cpp
+++ panelDriver.cpp
@@ -10,39 +10,6 @@
 
 #define TAG "esp32s3_rgbMatrix"
 
-//A, B, C, D must be on.config.pins.<40
-
-#define PIN_R1 GPIO_NUM_18
-#define PIN_G1 GPIO_NUM_8
-#define PIN_B1 GPIO_NUM_17
-#define PIN_R2 GPIO_NUM_16
-#define PIN_G2 GPIO_NUM_19
-#define PIN_B2 GPIO_NUM_15
-#define EXTRA_SPI_DATAPIN_1 GPIO_NUM_3
-#define EXTRA_SPI_DATAPIN_2 GPIO_NUM_45
-#define NUM_DATA_PINS 6
-#define PIN_A GPIO_NUM_7
-#define PIN_B GPIO_NUM_9
-#define PIN_C GPIO_NUM_6
-#define PIN_D GPIO_NUM_10
-#define PIN_E GPIO_NUM_20
-#define NUM_ROW_PINS 5
-#define PIN_OE GPIO_NUM_4
-#define PIN_LAT GPIO_NUM_11
-#define PIN_CLK GPIO_NUM_5
-
-#define PANEL_FRAMERATE (60)
-#define PANEL_NUM_ELECTRICAL_ROWS (8)
-#define PANEL_ELECTRICAL_ROW_LENGTH (256) //top half of panel only, as we have two sets of rgb pins
-#define PLANE_SIZE (PANEL_NUM_ELECTRICAL_ROWS*PANEL_ELECTRICAL_ROW_LENGTH)
-#define BUFFER_NUM_PLANES (4)
-#define BUFFER_SIZE (BUFFER_NUM_PLANES*PLANE_SIZE)
-
-#define PANEL_PX_WIDTH (128)
-#define PANEL_PX_HEIGHT (32)
-
-#define SPI_CLOCK_HZ (20 * 1000000) //20MHz
-#define SPI_CS_PIN 17
 
     static inline __attribute__((always_inline)) 
 void directWriteLow(uint8_t pin) 
